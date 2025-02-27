584fdf51899c2e006af3bb16d18dbd234aae77cd
oguz-y <oguzyorur@posta.mu.edu.tr>
2025-02-27 13:13:04 +0300

    Add files via upload

---
+++ Dt.java
@@ -0,0 +1,19 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Dt {
+
+    public static void main(String[] args) {
+        int grade = 1;
+        double grade_double = 1.5;
+        char first = 'A';
+        boolean isok = true ;
+
+        System.out.println(grade);
+        System.out.println(grade_double);
+        System.out.println(first);
+        System.out.println(isok);
+
+
+    }
+
+}
